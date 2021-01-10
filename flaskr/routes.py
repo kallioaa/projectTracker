5cac64a72725965447084b365e8e668175bd4c83
kallioaa <aatu.kallio@aalto.fi>
2021-01-10 21:22:03 +0200

    Login website

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,7 +1,18 @@
 from app import app
-
+from flask import render_template, redirect, request, session, url_for
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template("index.html")
 
+
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    if request.method == "POST":
+        return redirect(url_for("index"))
+    else: 
+        return render_template("login.html")  
+
+@app.route("/create_user")
+def create_user():
+    return render_template("newUser.html")
